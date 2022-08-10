bba6562f25a6941cc4f7f8ec3daddcbd46a34ca6
Diego Fernando Caviedes Camacho <diego_1996.c.c@hotmail.com>
2022-08-10 13:40:16 -0500

    feat: convert temperature Celcius to Fahrenheit

--- src/app/temperatureConverter/temperatureConverter.component.ts
+++ src/app/temperatureConverter/temperatureConverter.component.ts
@@ -7,10 +7,23 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 
 export class TemperatureConverter implements OnInit {
-
+  c = "";
+  f = "";
   ngOnInit() {
     // C = (F − 32) × 5/9
     // F = C*9/5 + 32
+  }
+
+  onCahange(value:string, type: "c" | "f"){
+    if(value === null){
+      return;
+    }
+    const temperature = Number(value);
+    if (type === "c"){ 
+      this.f =  ((temperature * 9) / 5 + 32).toFixed(1);
+    }else {
+      this.c = (((temperature - 32 )*5) /9).toFixed(1);
+    }
   }
 
 }
